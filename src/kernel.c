fc257d679826797300ca751e1847408411440290
bradford lamson-scribner <brad.lamson@gmail.com>
2018-09-06 21:04:49 -0600

    log the current exception level on startup

--- src/kernel.c
+++ src/kernel.c
@@ -3,7 +3,10 @@
 void kernel_main(void)
 {
     uart_init();
-    uart_send_string("Hello, world!\r\n");
+    init_print(0, putc);
+
+    int el = get_el();
+    printf("Exception Level: %d \r\n", el);
 
     while(1) {
         uart_send(uart_recv());
